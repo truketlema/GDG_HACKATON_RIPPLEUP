dce6da9eabae177b93de646c721264ce3e9f83c7
Hiruy Habtamu <hiruyhabtamu12@gmail.com>
2025-05-05 10:57:54 +0300

    Adds package management endpoints
    Introduces serializers, views, and URL routes for managing packages, including listing and detail views.
    
    Requires authentication for accessing package endpoints.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import SignupCreateAPIView,LoginAPIView
+from .views import SignupCreateAPIView,LoginAPIView,PackageListView,PackageDetailView
 
 urlpatterns = [
     path('signup/', SignupCreateAPIView.as_view(), name='signup'),
     path('login/', LoginAPIView.as_view(), name='login'),
+    path('packages/', PackageListView.as_view(), name='package-list'),
+    path('packages/<int:id>/', PackageDetailView.as_view(), name='package-detail'),
 ]
